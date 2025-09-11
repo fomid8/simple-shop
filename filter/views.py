7b2df68a8e0e4a43cc5ccafdcf3a2694f46136a9
fomid8 <fati.omid2000@gmail.com>
2025-09-11 12:41:58 +0330

    create apps

---
+++ filter/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+from django.http import *
+from .models import *
+from django.views import *
+
+# Create your views here.
+
+def hell(requst):
+    return HttpResponse('hello')
+
+
+
+'''
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.prefetch_related('images').all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ProductFilter
+    pagination_class = DefaultPagination
+    permission_classes = [IsAdminOrReadOnly]
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
+            return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        return super().destroy(request, *args, **kwargs)
+'''
